33e9c9416bbefa73154b672e22a378a804a59cf5
sebas <sebas@LAPTOP-PALU9LV1>
2021-05-31 00:29:28 -0300

    task : busqueda

--- grupoSGCR-tp8/src/main/java/ar/edu/unju/fi/tp8/controller/CompraController.java
+++ grupoSGCR-tp8/src/main/java/ar/edu/unju/fi/tp8/controller/CompraController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.unju.fi.tp8.service.IClienteService;
@@ -114,4 +115,10 @@ public class CompraController {
 		
 		return modelView;
 }
+	@GetMapping("/compra-busqueda")
+	public String buscarCompraPorFiltro(@RequestParam(name="nombre")String nombre, @RequestParam(name="precio")double precio,Model model, @ModelAttribute(name="compra") Compra compra){
+	    model.addAttribute("compra", compraService.getAllCompras());
+	    model.addAttribute("compras", compraService.buscarProductos(nombre, precio));
+	    return "compras";
+	}
 }
